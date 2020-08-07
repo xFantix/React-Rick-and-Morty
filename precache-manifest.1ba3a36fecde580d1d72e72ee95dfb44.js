15304b5ca53d83dcf7697ca5727ff555fd5fcd8a
Kamil Jankowski <kamil.jankoowski@outlook.com>
2020-08-07 12:45:56 +0200

    Updates

---
+++ precache-manifest.1ba3a36fecde580d1d72e72ee95dfb44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5400775d2be5047e0d6315058c0c32d9",
+    "url": "/React-Rick-and-Morty/index.html"
+  },
+  {
+    "revision": "f24e88459052d72cc1bd",
+    "url": "/React-Rick-and-Morty/static/css/main.3ffcaff5.chunk.css"
+  },
+  {
+    "revision": "094dc2bdccf2deeb7427",
+    "url": "/React-Rick-and-Morty/static/js/2.e2f82d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Rick-and-Morty/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f24e88459052d72cc1bd",
+    "url": "/React-Rick-and-Morty/static/js/main.bf03cf6c.chunk.js"
+  },
+  {
+    "revision": "2feb8bafed1f45a61690",
+    "url": "/React-Rick-and-Morty/static/js/runtime-main.613965bc.js"
+  }
+]);
